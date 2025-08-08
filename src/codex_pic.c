a8f3ad885e40797a31fdc05e5013a66876a9ea83
Urobobros <martin.burianek1987@gmail.com>
2025-08-08 23:37:24 +0200

    Replace PIC with proper 8259 implementation

--- src/codex_pic.c
+++ src/codex_pic.c
@@ -5,83 +5,51 @@
 
 void codex_pic_init(CodexPic* pic) {
     if (!pic) return;
-    memset(pic, 0, sizeof(*pic));
-    pic->imr_master = 0xFF;
-    pic->imr_slave  = 0xFF;
-    pic->vector_master = 0x08; /* default IRQ0 vector */
-    pic->vector_slave  = 0x70; /* typical PC BIOS value */
+    pic8259_reset(&pic->master);
+    pic8259_reset(&pic->slave);
+    pic->slave.vector_base = 0x70; // typical PC BIOS default after init
 }
 
 void codex_pic_io_write(CodexPic* pic, uint16_t port, uint8_t value) {
     if (!pic) return;
-    switch (port) {
-    case 0x20: /* master command */
-        if (value & 0x10)
-            pic->expect_icw_master = 1; /* expect ICW2 on data port */
-        /* ignore OCW2/OCW3 for now */
-        break;
-    case 0x21: /* master data */
-        if (pic->expect_icw_master) {
-            pic->vector_master = value & 0xF8;
-            pic->expect_icw_master = 0;
-        } else {
-            pic->imr_master = value;
-        }
-        break;
-    case 0xA0: /* slave command */
-        if (value & 0x10)
-            pic->expect_icw_slave = 1;
-        break;
-    case 0xA1: /* slave data */
-        if (pic->expect_icw_slave) {
-            pic->vector_slave = value & 0xF8;
-            pic->expect_icw_slave = 0;
-        } else {
-            pic->imr_slave = value;
-        }
-        break;
-    default:
-        break;
+    if (port >= 0x20 && port <= 0x21) {
+        pic8259_write(&pic->master, port, value);
+    } else if (port >= 0xA0 && port <= 0xA1) {
+        pic8259_write(&pic->slave, port, value);
     }
 }
 
 uint8_t codex_pic_io_read(CodexPic* pic, uint16_t port) {
-    if (!pic) return 0;
-    switch (port) {
-    case 0x20:
-        return 0; /* IRR not implemented */
-    case 0x21:
-        return pic->imr_master;
-    case 0xA0:
-        return 0; /* IRR not implemented */
-    case 0xA1:
-        return pic->imr_slave;
-    default:
-        return 0;
+    if (!pic) return 0xFF;
+    if (port >= 0x20 && port <= 0x21) {
+        return pic8259_read(&pic->master, port);
+    } else if (port >= 0xA0 && port <= 0xA1) {
+        return pic8259_read(&pic->slave, port);
     }
+    return 0xFF;
 }
 
 void codex_pic_raise_irq(CodexPic* pic, struct CodexCore* core, uint8_t irq) {
     if (!pic || !core) return;
-    uint8_t vector;
+
     if (irq < 8) {
-        if (pic->imr_master & (1u << irq)) return; /* masked */
-        vector = pic->vector_master + irq;
-    } else {
-        uint8_t line = irq - 8;
-        if (pic->imr_slave & (1u << line)) return;
-        if (pic->imr_master & (1u << 2)) return; /* cascade masked */
-        vector = pic->vector_slave + line;
-    }
+        pic8259_raise_irq(&pic->master, irq);
+        if (!pic8259_has_pending_unmasked(&pic->master))
+            return;
+        uint8_t vector = pic8259_acknowledge(&pic->master);
 #ifdef _WIN32
-    WHV_INTERRUPT_CONTROL ctrl;
-    memset(&ctrl, 0, sizeof(ctrl));
-    ctrl.Type = WHvX64InterruptTypeFixed;
-    ctrl.Vector = vector;
-    ctrl.TargetVtl = 0;
-    WHvRequestInterrupt(core->partition, &ctrl, sizeof(ctrl));
+        WHV_INTERRUPT_CONTROL ctrl;
+        memset(&ctrl, 0, sizeof(ctrl));
+        ctrl.Type = WHvX64InterruptTypeFixed;
+        ctrl.Vector = vector;
+        ctrl.TargetVtl = 0;
+        WHvRequestInterrupt(core->partition, &ctrl, sizeof(ctrl));
 #else
-    (void)vector;
+        (void)vector; (void)core;
 #endif
+    } else {
+        // IRQs >=8 go to the slave. Devices using them are not yet modelled.
+        pic8259_raise_irq(&pic->slave, irq - 8);
+    }
 }
 
---
+++ src/pic8259.c
@@ -0,0 +1,124 @@
+#include "pic8259.h"
+
+#include <string.h>
+
+// ---- helpers ----
+static int highest_priority_line(uint8_t bits, uint8_t prio_add) {
+    if (!bits) return -1;
+    for (int i = 0; i < 8; ++i) {
+        int line = (i + prio_add) & 7;  // rotated priority
+        if (bits & (1u << line)) return line;
+    }
+    return -1;
+}
+
+static int highest_inservice_line(const PIC8259* p) {
+    return highest_priority_line(p->isr, p->priority_add);
+}
+
+// ---- public API ----
+void pic8259_reset(PIC8259* p) {
+    memset(p, 0, sizeof(*p));
+    p->vector_base = 0x08;      // XT default after init (ICW2 will override)
+    p->read_isr    = 0;         // read IRR by default
+    p->priority_add = 0;        // IRQ0 highest
+}
+
+void pic8259_raise_irq(PIC8259* p, int line) {
+    uint8_t m = 1u << (line & 7);
+    if (!(p->last_level & m)) {
+        p->irr |= m;
+        p->last_level |= m;
+    }
+}
+
+void pic8259_lower_irq(PIC8259* p, int line) {
+    p->last_level &= (uint8_t)~(1u << (line & 7));
+}
+
+bool pic8259_has_pending_unmasked(const PIC8259* p) {
+    return (p->irr & (uint8_t)~p->imr) != 0;
+}
+
+int pic8259_get_pending_line(const PIC8259* p) {
+    return highest_priority_line((uint8_t)(p->irr & (uint8_t)~p->imr), p->priority_add);
+}
+
+uint8_t pic8259_acknowledge(PIC8259* p) {
+    int line = pic8259_get_pending_line(p);
+    if (line < 0) return 0xFF; // no vector
+
+    uint8_t m = (uint8_t)(1u << line);
+    p->irr &= (uint8_t)~m;
+
+    if (!p->auto_eoi) {
+        p->isr |= m;           // in-service until EOI
+    }
+    return (uint8_t)(p->vector_base + line);
+}
+
+void pic8259_eoi(PIC8259* p, int line) {
+    int l = line;
+    if (l < 0) l = highest_inservice_line(p);
+    if (l < 0) return;
+    p->isr &= (uint8_t)~(1u << l);
+}
+
+uint8_t pic8259_read(PIC8259* p, uint16_t port) {
+    if ((port & 1) == 0) {
+        return p->read_isr ? p->isr : p->irr;
+    } else {
+        return p->imr;
+    }
+}
+
+void pic8259_write(PIC8259* p, uint16_t port, uint8_t val) {
+    if ((port & 1) == 0) { // command
+        if (val & PIC8259_ICW1_INIT) {
+            p->icw_step    = 2;
+            p->expect_icw4 = (val & PIC8259_ICW1_ICW4) ? 1 : 0;
+            p->imr = p->irr = p->isr = p->last_level = 0;
+            p->priority_add = 0;
+            p->read_isr = 0;
+            return;
+        }
+        if ((val & 0x18) == 0x00) {
+            if (val & PIC8259_OCW3_SET_RIS)
+                p->read_isr = (uint8_t)(val & PIC8259_OCW3_RIS);
+        } else {
+            uint8_t cmd = (val & 0xE0);
+            uint8_t lvl = (uint8_t)(val & 0x07);
+            switch (cmd) {
+                case PIC8259_OCW2_NON_SPEC_EOI:
+                    pic8259_eoi(p, -1);
+                    break;
+                case PIC8259_OCW2_SPEC_EOI:
+                    pic8259_eoi(p, lvl);
+                    break;
+                case PIC8259_OCW2_ROT_NON_SPEC:
+                    pic8259_eoi(p, -1);
+                    p->priority_add = (uint8_t)((p->priority_add + 1) & 7);
+                    break;
+                case PIC8259_OCW2_ROT_SPEC:
+                    pic8259_eoi(p, lvl);
+                    p->priority_add = (uint8_t)((lvl + 1) & 7);
+                    break;
+                default:
+                    break;
+            }
+        }
+    } else { // data
+        if (p->icw_step == 2) {
+            p->vector_base = (uint8_t)(val & 0xF8);
+            p->icw_step = p->expect_icw4 ? 4 : 0;
+        } else if (p->icw_step == 3) {
+            p->icw_step = p->expect_icw4 ? 4 : 0;
+        } else if (p->icw_step == 4) {
+            p->auto_eoi = (uint8_t)((val & PIC8259_ICW4_AUTO_EOI) ? 1 : 0);
+            p->icw_step = 0;
+        } else {
+            p->imr = val;
+        }
+    }
+}
+
